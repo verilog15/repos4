2c50d2bacf6f915e783c9b0d6d8181ec34479895
verilog15 <verilog15@users.noreply.github.com>
2025-04-16 22:30:12 +0300

    Add merbanan/rtl_433 at 1744831760

---
+++ 1744831760-merbanan-rtl_433/include/decoder.h
@@ -0,0 +1,17 @@
+/** @file
+    Meta include for all decoders.
+*/
+
+#ifndef INCLUDE_DECODER_H_
+#define INCLUDE_DECODER_H_
+
+#include <string.h>
+#include <stdio.h>
+#include "r_device.h"
+#include "bitbuffer.h"
+#include "data.h"
+#include "bit_util.h"
+#include "decoder_util.h"
+#include "c_util.h"
+
+#endif /* INCLUDE_DECODER_H_ */
